d60d3db8c175bd0959d4697ebba01946e456cb29
Jérémie FOURBIL <jeremie.fourbil@gmail.com>
2012-08-14 18:04:26 +0200

    minor fixes

--- server/server.js
+++ server/server.js
@@ -22,42 +22,89 @@
 /*jshint node:true*/
 'use strict';
 (function(){
-
 // Modules dependencies
-var express = require('express');
-var app = express.createServer();
-var path = require('path');
+var express = require('express'),
+    connect = require('connect'),
+    https = require('https'),
+    // app = express.createServer(),
+    path = require('path'),
+    mongo = require('mongodb'),
+    mongoose = require('mongoose'),
+    MongoStore = require('connect-mongodb'),
+    jade = require('jade'),
+    fs = require('fs'),
+    options = {
+        key: fs.readFileSync(__dirname+'/key.pem'),
+        cert: fs.readFileSync(__dirname+'/cert.pem'),
+        ca: fs.readFileSync(__dirname+'/cert.csr')
+    };
+    // models = require('./models');
 
 // Configuration
-var port = 3000;
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname +'/..'));
-});
+var port = 443,
+    User,
+    app,
+    serverapp,
+    // LoginToken,
+    Server = mongo.Server,
+    Db = mongo.Db,
+    server_config = new Server('localhost', 27017, {auto_reconnect: true, native_parser: true}),
+    db = new Db('focusmatic', server_config, {});
+    
+app = express();
+serverapp = https.createServer(options, app);
 app.configure('development', function(){
-
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+
 app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
-// Routes
-app.get('/', function(req, res){
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m \x1b[34m:status\x1b[0m :response-time ms' }));
 
+    app.use(express.cookieParser('focusmatic'));
+    app.use(express.session({
+        cookie : {maxAge: 60000 * 1}, // 1 minute
+        store: new MongoStore({db : db})
+    }));
+
+
+    app.use(app.router);
+    app.use('/shared', express.static(__dirname + '/../shared'));
+    app.use(express.static(__dirname +'/../client'));
+});
+
+// load login module
+
+var LM = require('./login');
+
+
+// Routes
+
+app.get('/login', LM.routes.loginGET );
+app.post('/login', LM.routes.loginPOST);
+app.get('/users',LM.routes.loadUser , LM.routes.list);
+app.get('/user', LM.routes.loadUser, LM.routes.user);
+app.get('/user/:id', LM.routes.loadUser, LM.routes.user);
+app.get('/users/add', LM.routes.signin);
+app.post('/users/add', LM.routes.add);
+app.get('/logout', LM.routes.loadUser, LM.routes.logout);
+app.get('/', function (req, res, next){
     res.sendfile(path.normalize(__dirname+'/../client/index.html'));
 });
 
 app.get('/test', function(req, res){
-	res.sendfile(path.normalize(__dirname+'/../client/index_test.html'));
+    res.sendfile(path.normalize(__dirname+'/../client/index_test.html'));
 });
 
-app.listen(port, function() {
+/////////////////////////////////////////////////////////
+
+serverapp.listen(port, function() {
     console.log("Listening on " + port);
 });
 console.log("Express server listening on port %d in %s mode", port, app.settings.env);
-
-
 })();
